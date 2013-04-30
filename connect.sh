8e40ab8b56df240fc28459c6f5f0cfd613e050ef
Marc Young <myoung34@my.apsu.edu>
2013-04-29 22:54:43 -0500

    User should be dynamic, and this should be forced to run as root

--- connect.sh
+++ connect.sh
@@ -5,25 +5,26 @@
 #---------------------------------------------------------------
 # Credit to Ric123 and megamanextent for the primer material
 # found at UbuntuForums.Org
+# Credit to Marcus Young <myoung34@my.apsu.edu> for a refactor
 #---------------------------------------------------------------
 # Based on the work of Alex Zaballa (02/24/2011)
 # Distributed under GPL v.2.
 #---------------------------------------------------------------
 
 #clear
-
+rebootDisconnect=true #only enable this if a reboot of the pi is needed on disconnect
 paID=""
 qPath=""
-
+user="pi"
 
 unmute() {
 	echo "Unmuting"
-	su pi -c 'pacmd "set-sink-mute 0 0"'
+	su $user -c 'pacmd "set-sink-mute 0 0"'
 }
 
 mute() {
 	echo "Muting"
-	su pi -c 'pacmd "set-sink-mute 0 1"'
+	su $user -c 'pacmd "set-sink-mute 0 1"'
 }
 
 connect() {
@@ -44,34 +45,37 @@ connect() {
 	unmute
 
 	# Return Example: bluez_source.01_23_45_AB_CD_EF
-	bluezSource="$(su pi -c "pactl list" | grep -m 1 "Name: bluez_source" | cut -c 8-)"
+	bluezSource="$(su $user -c "pactl list" | grep -m 1 "Name: bluez_source" | cut -c 8-)"
 	echo "[Connected] Bluez Source: ${bluezSource}"
 
 	# Return Example: alsa_output.pci-0000_00_10.1.analog-stereo
-	alsaSink="$(su pi -c "pactl list" | grep -m 1 "Name: alsa_output" | cut -c 8-)"
+	alsaSink="$(su $user -c "pactl list" | grep -m 1 "Name: alsa_output" | cut -c 8-)"
 	echo "[Connected] Alsa Sink: ${alsaSink}"
 
 	# Return Example: 25
-	paID=$(su pi -c "pactl load-module module-loopback source=${bluezSource} sink=${alsaSink}")
+	paID=$(su $user -c "pactl load-module module-loopback source=${bluezSource} sink=${alsaSink}")
 	echo "[Connected] pactl ID number: ${paID}"
 }
 
 disconnect() {
- 
-	mute
-	echo "[Disconnected] Unloading module: ${paID}"
-	su pi -c "pactl unload-module ${paID}"
-	qPath="$(qdbus --system org.bluez | grep -m 1 "/dev_")"
-	qdbus --system org.bluez "${qPath}" org.bluez.AudioSource.Disconnect 1> /dev/null
-	echo "[Disconnected] Device disconnected, restarting..."
-      
+ 	mute
+  echo "[Disconnected] Unloading module: ${paID}"
+ 	su $user -c "pactl unload-module ${paID}"
+  qPath="$(qdbus --system org.bluez | grep -m 1 "/dev_")"
+ 	qdbus --system org.bluez "${qPath}" org.bluez.AudioSource.Disconnect 1> /dev/null
+  echo "[Disconnected] Device disconnected, restarting..."
+
+  if $rebootDisconnect ;
+  then 
+    reboot
+  fi
 }
 
 main() {
 	echo "Waiting for connection..."
 	while :
 	do
-		qPath="$(su pi -c 'pactl list' | grep -m 1 'Name: bluez_source' | cut -c 8-)"
+		qPath="$(su $user -c 'pactl list' | grep -m 1 'Name: bluez_source' | cut -c 8-)"
 		if [ "$qPath" != ""  ]
 		then
 			echo "Found ${qPath}, connecting..."
@@ -85,6 +89,12 @@ main() {
 
 }
 
+
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
 # Signal handling allows us to 
 # Cleanly exit the program.
 trap mute EXIT
@@ -94,7 +104,7 @@ main
 
 while :
 do
-	qPath="$(su pi -c 'pactl list' | grep -m 1 'Name: bluez_source' | cut -c 8-)"
+	qPath="$(su $user -c 'pactl list' | grep -m 1 'Name: bluez_source' | cut -c 8-)"
 
 	if [ -z "${qPath}" ]
 	then
--- startup.sh
+++ startup.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+user="pi"
 #cd "$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR=$( readlink -f "$( dirname "$0" )")
 cd "$( cd -P $DIR && pwd )"
@@ -7,7 +13,7 @@ cd "$( cd -P $DIR && pwd )"
 #modprobe snd_bcm2835
 
 # Start pulseaudio
-su pi -c 'pulseaudio --start'
+su $user -c 'pulseaudio --start'
 
 # Set up the bluetooth adapter to be as visible as possible.
 hciconfig hci0 piscan
